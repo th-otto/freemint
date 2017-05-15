fdc1333a4928d20578af12068894031bd14f1035
Miro Kropacek <miro.kropacek@gmail.com>
2017-05-15 12:18:50 +1000

    Introduce separate old/new builds
    Old build uses -CUR again and it's built as an independent instance. That's why we use 'new' and 'old' subdirectories so we wont overwrite already pushed build.

---
+++ .travis/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Use as ". build.sh"
+
+TMP="$1"
+OUT="$2"
+
+if [ -z "$OLD_BUILD" -o "$OLD_BUILD" -eq 0 ]
+then
+	export PUBLISH_PATH="${PUBLISH_PATH}/new"
+
+	# TODO: when all the tools (mintloader etc) are added to 1.18 branch
+else
+	export PUBLISH_PATH="${PUBLISH_PATH}/old"
+
+	mkdir -p "${OUT}"
+	"./.travis/freemint.org/freemint.build" "${OUT}"
+fi
